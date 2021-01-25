2005b107ff15fb57fc82b2f41a2d9bedec0b6274
kasahun2321 <59380190+kasahun2321@users.noreply.github.com>
2021-01-25 00:22:32 -0600

    new work on database

---
+++ Desktop/hmtl/jsfolder/day1.js
@@ -0,0 +1,58 @@
+// /// variable declaration
+// // es6 
+// let x = 2;
+// console.log("the value of x=", x)
+// console.log(typeof (x))
+// const name = "mengestu";
+// console.log("your name is =", name)
+// console.log(typeof (name))
+
+// ///////////old way never 
+// var myfullname = "mengest ...." //very bad  way 
+
+// //function // is a block of code which works specfic task
+
+// function sum(a,b)
+// {
+//     return a + b;
+// }
+// function mult(a,b)
+// {
+//     return a * b;
+// }
+// function div(a,b)
+// {
+//     return a / b;
+// }
+// function sub(a,b)
+// {
+//     return a - b;
+// }
+
+// let a = 33;
+// let b = 12;
+
+// // calling the function or invoking the
+// let sumresult = sum(a, b);
+// console.log(sumresult)
+// let divresult = div(a, b)
+// console.log(divresult)
+// console.log( mult(44,89),sub("kasahun", b))
+
+
+
+// // key  word 
+//  //=var , let , function , for , if , else . switch ,and , or , while, do, for each, array , 
+
+
+// let prompt = require('prompt-sync')();
+let r = prompt('Eneter value in celsius: ')
+let height = prompt("height")
+let volume=math.pi *r**2*height
+console.log(volume);
+
+let x = 3;
+x++
+console.log(x);//3
+
+
---
+++ Desktop/hmtl/kasa.js
@@ -0,0 +1,39 @@
+var X = 5;
+let y = 50;
+let sum = X + y;
+let result = add(12, 34);
+function add(kasahun, mengae) {
+    // display here
+    console.log(kasahun + mengae)
+    // return value to the caller 
+    return kasahun + mengae;
+}
+console.log(result);
+
+
+///html 30 tag yalew and page tag with its function 
+// navbar , div , image, a, list(ordered, undored list tage)
+
+
+///////////////js 
+function add(x, y)
+{
+    if (x+y==6, 8, 7 good)
+    {
+        console.log("confusing numbet")
+    }
+    if (x + y == 7)
+    {
+      goof  
+    }
+   // 8 confusing
+    
+    return x+y
+}
+
+function multy(x,u)
+{
+
+}
+div()
+subtract()
